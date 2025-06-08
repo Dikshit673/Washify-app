0f0de452fa50b0ddb7a46e62c35955dbbb609bce
dikshit673 <sachinnavodian@gmail.com>
2025-06-09 00:39:20 +0530

    Arred barrel import for home route data

---
+++ src/pages/Home/data/index.ts
@@ -0,0 +1,5 @@
+export { galleryArr } from './galleryArr';
+export { testimonialArr } from './testimonialArr';
+export { TimeOptionsArr } from './ContactTimeOptionsArr';
+export { heroArr } from './heroArr';
+export { plansArr } from './plansArr';

3c95c8c3636c7c210ffc06356630748f2d3ac620
dikshit673 <sachinnavodian@gmail.com>
2025-06-09 00:37:02 +0530

    Added barrel import for src/components/ui

---
+++ src/components/ui/index.ts
@@ -0,0 +1,6 @@
+export { Button } from './Button';
+export { Carousel } from './Carousel';
+export { Heading } from './Heading';
+export { Input, TextArea, Select, Option } from './Input';
+export { Link } from './Link';
+export { StarRating } from './StarRating';

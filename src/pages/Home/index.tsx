b42efc1ee2777763640bb249247a2b13692a15a2
dikshit673 <sachinnavodian@gmail.com>
2025-06-09 00:43:17 +0530

    Added all section in home route

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,34 @@
+import Footer from '@/components/Footer';
+import Navbar from '@/components/Navbar';
+
+import Hero from './components/Hero';
+import About from './components/About';
+import Contact from './components/Contact';
+import Gallery from './components/Gallery';
+import Plans from './components/Plans';
+import Services from './components/Services';
+import Testimonials from './components/Testimonials';
+import Location from './components/Location';
+
+const Home = () => {
+  return (
+    <>
+      <Navbar />
+      <main className='*:odd:bg-second-300'>
+        <Hero />
+        <About />
+        <Services />
+        <Plans />
+        <Contact />
+        <Testimonials />
+        <Gallery />
+        <Location />
+        {/* <NewsLetter /> */}
+      </main>
+
+      <Footer />
+    </>
+  );
+};
+
+export default Home;
